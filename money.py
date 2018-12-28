882089111513a0b14ee313086dcb944c9d36d7e2
miller1674@hotmail.com <miller1674@hotmail.com>
2018-12-28 02:37:27 -0500

    fixed bug causing crashes and some errors

--- money.py
+++ money.py
@@ -10,9 +10,10 @@ class Money:
     def buy(self, price, number):
         self._curr_money=self._curr_money-(price*number)
     
-    def sell(self,price, number)
+    def sell(self,price, number):
         self._curr_money=self._curr_money+(price*number)
         
     
-    def gains(self):
-        print((self._curr_money-self._start_money)/self._curr_money)
+    def gains(self, portfolio):
+        total=self._curr_money+(portfolio[0]._currPrice*portfolio[0]._number_owned)+(portfolio[1]._currPrice*portfolio[1]._number_owned)+(portfolio[2]._currPrice*portfolio[2]._number_owned)
+        print("Percent Gain: "+(str(((total-self._start_money)/total)*100)))
